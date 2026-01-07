2c678317217ce9d8719a9f0bec4a68d6de20f5c9
Abdulla Ahmed <adduahm@gmail.com>
2026-01-07 14:46:22 +0530

    Add geolocation feature to display user location

--- js/script.js
+++ js/script.js
@@ -15,3 +15,33 @@ L.marker(iiitdm)
     .addTo(map)
     .bindPopup("IIITDM Kurnool Campus")
     .openPopup();
+
+let userMarker = null;
+
+document.getElementById("locateBtn").addEventListener("click", function () {
+    if (!navigator.geolocation) {
+        alert("Geolocation is not supported by your browser");
+        return;
+    }
+
+    navigator.geolocation.getCurrentPosition(function (position) {
+        const lat = position.coords.latitude;
+        const lng = position.coords.longitude;
+
+        const userLocation = [lat, lng];
+
+        if (userMarker) {
+            map.removeLayer(userMarker); // remove old marker
+        }
+
+        userMarker = L.marker(userLocation)
+            .addTo(map)
+            .bindPopup("📍 You are here")
+            .openPopup();
+
+        map.setView(userLocation, 18);
+    }, function () {
+        alert("Unable to get your location");
+    });
+});
+
